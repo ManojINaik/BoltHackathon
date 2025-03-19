c9662ca720d19fba3d8cb72c0fb6ec9f2d4c1dc5
Manoj <naik97059@gmail.com>
2025-03-20 01:07:53 +0530

    updated model

--- src/sections/Leaders/constants.ts
+++ src/sections/Leaders/constants.ts
@@ -5,28 +5,10 @@ import {
   BradenReam,
   DanielleStrachman,
   DeonNicholas,
-  DhananjaJayalath,
   EdwardKim,
-  FelipePerez,
   HenryShi,
   JenDewalt,
-  KanwarSahdra,
-  KelvinZhang,
-  LucilleTasker,
-  MayuranYogarajah,
-  MichaelGibson,
-  MikeKirkup,
-  NabilFahel,
-  PierreArys,
-  PuryaSarmadi,
-  SamridhSaluja,
-  ShauryaSaluja,
-  SrijitMitra,
   SulmanChoudhry,
-  SwadhaRai,
-  TomAlterman,
-  VartikaManasvi,
-  ZvezdinBesarabov,
 } from "src/assets/img";
 
 export const leaders: {
@@ -65,19 +47,9 @@ export const leaders: {
     avatar: DeonNicholas,
   },
   {
-    name: "Dhananja Jayalath (DJ)",
-    title: "Co-founder, Athos",
-    avatar: DhananjaJayalath,
-  },
-  {
     name: "Edward Kim",
     title: "Manager of Technical Staff, Cohere",
     avatar: EdwardKim,
-  },
-  {
-    name: "Felipe Perez",
-    title: "VP of Machine Learning, Signal 1",
-    avatar: FelipePerez,
   },
   {
     name: "Henry Shi",
@@ -90,84 +62,8 @@ export const leaders: {
     avatar: JenDewalt,
   },
   {
-    name: "Kanwar Sahdra",
-    title: "Software Engineer, Coinbase",
-    avatar: KanwarSahdra,
-  },
-  {
-    name: "Kelvin Zhang",
-    title: "Member of Technical Staff, Endex",
-    avatar: KelvinZhang,
-  },
-  {
-    name: "Lucille Tasker",
-    title: "Sr. Director of Data and Technology, Rewriting the Code",
-    avatar: LucilleTasker,
-  },
-  {
-    name: "Mayuran Yogarajah",
-    title: "Founding CTO, Index Exchange",
-    avatar: MayuranYogarajah,
-  },
-  {
-    name: "Michael Gibson",
-    title: "Co-founder & GP, 1517 Fund",
-    avatar: MichaelGibson,
-  },
-  { name: "Mike Kirkup", title: "CTO, EnPowered", avatar: MikeKirkup },
-  {
-    name: "Nabil Fahel",
-    title: "Executive VP, StartupSoft.com",
-    avatar: NabilFahel,
-  },
-  {
-    name: "Pierre Arys",
-    title: "Partner, Cherubic Ventures",
-    avatar: PierreArys,
-  },
-  {
-    name: "Purya Sarmadi",
-    title: "Co-founder & CEO, MedMe Health",
-    avatar: PuryaSarmadi,
-  },
-  {
-    name: "Samridh Saluja",
-    title: "Co-founder & CEO, Guardrail.ai",
-    avatar: SamridhSaluja,
-  },
-  {
-    name: "Shaurya Saluja",
-    title: "Founder & CEO, Fleet",
-    avatar: ShauryaSaluja,
-  },
-  {
-    name: "Srijit Mitra",
-    title: "Sr. Manager of Startups Solutions Architecture, AWS",
-    avatar: SrijitMitra,
-  },
-  {
     name: "Sulman Choudhry",
     title: "Head of Engineering, ChatGPT, OpenAI",
     avatar: SulmanChoudhry,
-  },
-  {
-    name: "Swadha Rai",
-    title: "Data Analyst, Rewriting the Code",
-    avatar: SwadhaRai,
-  },
-  {
-    name: "Tom Alterman",
-    title: "Head of Product, Notable",
-    avatar: TomAlterman,
-  },
-  {
-    name: "Vartika Manasvi",
-    title: "Serial Entrepreneur & Investor, MaRS & Communitech",
-    avatar: VartikaManasvi,
-  },
-  {
-    name: "Zvezdin Besarabov",
-    title: "Co-founder, Outdid.io",
-    avatar: ZvezdinBesarabov,
   },
 ];
