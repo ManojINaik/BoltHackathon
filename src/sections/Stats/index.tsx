4eddb7d87ad7f8176192e72f162044a7e415ae17
Manoj <naik97059@gmail.com>
2025-03-20 00:59:18 +0530

    updated model

--- src/sections/Stats/index.tsx
+++ src/sections/Stats/index.tsx
@@ -229,7 +229,7 @@ const Stats = (): JSX.Element => {
               height="100%"
               position={[0, -0.4, 0]}
               rotation={[0.15, 0, 0]}
-              scale={10.0}
+              scale={12.5}
               autoRotate={true}
               animationName={
                 USE_ANIMATION && laptopInView ? "Animation" : undefined
